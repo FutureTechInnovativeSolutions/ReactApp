895c20332f7a7adfba4cbf06cdf68f4b33db2f26
FutureTechInnovativeSolutions <futuretechinnovativesolutions@gmail.com>
2023-04-02 19:51:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [data,setData] = useState({
+    email:'',
+    password:''
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <form>
+          <label>E-Mail</label><br/>
+          <input type="text" /> <br/>
+          <label>Password</label> <br/>
+          <input type="password"/> <br/> 
+          <input type="submit" value="Login"/> 
+        </form>
+      </center>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
